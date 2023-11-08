8716ca0e7d544718eeb25df13d35ce6de1ae6c0a
D1104182102 <d1104182102@gm.lhu.edu.tw>
2023-11-08 08:37:17 +0800

    新增關聯式表格定義

--- database/migrations/2023_10_11_064245_create_pokemons_table.php
+++ database/migrations/2023_10_11_064245_create_pokemons_table.php
@@ -15,12 +15,24 @@ class CreatePokemonsTable extends Migration
     {
         Schema::create('pokemons', function (Blueprint $table) {
             $table->id();
-            $table->string('pokemon');
-            $table->string('regions');
-            $table->string('regionss');
-            $table->string('gender');
-            $table->string('ability');
+            $table->string('pokemon')->comment('寶可夢(主鍵)');
+            $table->string('region')->nullable()->comment('地區');
+            $table->foreignId('tid1')->comment('屬性編號1(外部鍵)');
+            $table->foreign('tid1')
+                    ->references('id')
+                    ->on('types')
+                    ->onDelete('cascade');
+            $table->integer('tid2')->comment('屬性編號2(外部鍵)');
+            $table->foreign('tid2')
+                    ->references('id')
+                    ->on('types')
+                    ->onDelete('cascade');
+            $table->double('height')->nullable()->unsigned()->comment('身高');
+            $table->double('weight')->nullable()->unsigned()->comment('體重');
+            $table->string('gender')->nullable()->comment('性別');
+            $table->string('ability')->comment('特性');
             $table->timestamps();
+
         });
     }
 
